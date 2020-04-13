f95543d287ab31bba74cf18ab1d372f42a7a5eb5
K <kevinswho@gmail.com>
2020-04-12 21:39:42 -0400

    splash screen

---
+++ app/src/main/java/com/example/umdcsapp/DegreeTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.example.umdcsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DegreeTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_degree_tracker);
+    }
+}
